f256c3d455b5ce8b5b26f316bcac20cf71fc22a1
Mairon salazar <maironsalazar16@gmail.com>
2024-04-20 15:24:38 -0500

    continuar la solucion del ejercicio

--- condicionales/condicionales.py
+++ condicionales/condicionales.py
@@ -48,16 +48,43 @@
 "-------------------hacer un programa que pida tre numeros y determine el mayor----------------------------"
 
 
-num1 = int(input("ingrese un numero: "))
-num2 = int(input("ingrese el segundo numero: "))
-num3 = int(input("ingrese el tercer numero: "))
+# num1 = int(input("ingrese un numero: "))
+# num2 = int(input("ingrese el segundo numero: "))
+# num3 = int(input("ingrese el tercer numero: "))
 
 
-if num1 > num2 and num1 > num3:
-    print(f"el numero uno es mayorporque es {num1}")
-elif num2 > num1 and num2 > num3:
-    print (f"el numero dos es mayoro porque es {num2}")
-elif num3 > num1 and num3 > num2:
-    print(f"el numero 3 es mayor porque es {num3}")
-elif num1 == num2 and num2 == num3:
-    print("los tres on iguales ")
+# if num1 >= num2 and num1 >= num3:
+#     print(f"el numero uno es mayorporque es {num1}")
+# elif num2 >= num1 and num2 >= num3:
+#     print (f"el numero dos es mayoro porque es {num2}")
+# elif num3 >= num1 and num3 >= num2:
+#     print(f"el numero 3 es mayor porque es {num3}")
+# elif num1 == num2 and num2 == num3:
+#     print("los tres on iguales ")
+
+
+"-----------------------hace un programa que pida un caracter e indique si es una vocal o no--------------------------------"
+
+# vocal = input("escriber un caracter: ")
+
+# if vocal == "a":
+#     print(f"su caracter es una vocal {vocal}")
+# elif vocal == "e":
+#     print(f"su caracter es una vocal {vocal}")
+# elif vocal == "i":
+#     print(f"su caracter es una vocal {vocal}")
+# elif vocal == "o":
+#     print(f"su caracter es una vocal {vocal}")
+# elif vocal == "u":
+#     print(f"su caracter es una vocal {vocal}")
+# else:
+#     print("no es un vocal")
+
+"---------paso 2------------"
+
+letra = input("digite un caracter: ").lower()              #el .lower() nos convierte el caracter a minuscula si estas escribiendo en mayuscula y recordar que el upeer() la convierte a mayuscula 
+
+if letra == "a" or letra =="e" or letra =="i" or letra =="o" or letra == "u":
+    print ("es vocal")
+else:
+    ("no es una vocal")

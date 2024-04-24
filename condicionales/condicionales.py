01a9c75d6c0fda75818322eeb24e7c8f87f4da99
Mairon salazar <maironsalazar16@gmail.com>
2024-04-23 19:09:16 -0500

    ultimo ejercicio de condicionales

--- condicionales/condicionales.py
+++ condicionales/condicionales.py
@@ -33,16 +33,16 @@ y quiere calcular de forma automatica el precio que debe cobrar a sus clientes p
 y mostrar el precio de la entrada. si el cliente es menor de 4 años puede entrar gratis. si tiene entre 4 y 18 años debe pagar 5EUROS
 y si es mayor de 18 años debe pagar 18 euros'''
 
-edad = int(input("ingrese su edad: "))
+# edad = int(input("ingrese su edad: "))
 
-if edad == 0 and edad <= 4 :
-    print(f"usted tiene {edad} años y puede entrar gratis")
-elif edad > 4 and edad <18:
-    print(f"su edad es de {edad} años, debe pagar 5 EUROS en caja ")
-elif edad >= 18 and edad <100:
-    print(f"usted tiene {edad} años debe para 10 EUROS")
-else:
-    print(f"no puedes tener {edad} años")
+# if edad == 0 and edad <= 4 :
+#     print(f"usted tiene {edad} años y puede entrar gratis")
+# elif edad > 4 and edad <18:
+#     print(f"su edad es de {edad} años, debe pagar 5 EUROS en caja ")
+# elif edad >= 18 and edad <100:
+#     print(f"usted tiene {edad} años debe para 10 EUROS")
+# else:
+#     print(f"no puedes tener {edad} años es imposibrle ")
 
 
 '''hacer un programa que pida al usuario su edad y muestre por pantalla si es mayor de edad o no '''
@@ -237,3 +237,39 @@ escribir un programa que pregunte al usuario su edad y sus ingresos mensules y m
 #     print("la opcion no esta disponible intentalo denuevo ")
     
     
+'''La pizzería Bella Napoli ofrece pizzas vegetarianas y no vegetarianas a sus clientes. Los ingredientes para cada tipo de pizza aparecen a continuación.
+
+    Ingredientes vegetarianos: Pimiento y tofu.
+    Ingredientes no vegetarianos: Peperoni, Jamón y Salmón.
+
+Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o no, y en función de su respuesta le muestre un menú 
+con los ingredientes disponibles para que elija. Solo se puede eligir un ingrediente además de la mozzarella y el tomate que están en todas la pizzas. 
+Al final se debe mostrar por pantalla si la pizza elegida es vegetariana o no y todos los ingredientes que lleva.'''
+
+
+user = input("desea una piza  \nA) vegetariana o \nB) no vegetariana: ").upper()
+
+if user == "A" :
+    print('''usted eligio vegetariana marque
+          A PIMENTO
+          B TOFU''')
+    opcion = input("seleccione el ingrediente: ").upper()
+    if opcion == "A":
+        print(" su piza vegetariana llevara PIMENTON, Y en 5 minutos sale ")
+    elif opcion == "B":
+         print(" su piza vegetariana llevara TOFU, Y en 5 minutos sale ")
+if user == "B":
+    print('''usted eligio pizza no vegetaria marque 
+          A PEPERONI
+          B JAMON Y SALMON''')
+    opcion2 = input("seleccione el ingrediente: ").upper()
+    if opcion2 == "A":
+        print("su pizza tomate y mozzarela llervara peperoni en 5 minutos la entregamos")
+    elif opcion2 == "B":
+         print("su pizza tomate y mozzarela llervara JAMON Y SALMON y en 5 minutos la entregamos")
+    else:
+        print("no es una opcion correcta")
+else:
+    print("no es una opcion correcta")
+
+

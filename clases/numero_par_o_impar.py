cd2b805dd6628d4fd9a3ba4c0523c11ec33d119c
Mairon salazar <maironsalazar16@gmail.com>
2024-07-17 19:15:10 -0500

    operadores y ejerciciso

--- clases/numero_par_o_impar.py
+++ clases/numero_par_o_impar.py
@@ -1,3 +1,5 @@
+'''***************************** istema de numeros pares he impares **********************************'''
+
 '''se solicita crea un programa que indique si un numero es ar o impar\n'''
 
 print("sistema de numeros pares he impares")
@@ -8,14 +10,15 @@ print("sistema de numeros pares he impares")
 # else:
 #     print("el numero es impar:")
 
+'''***************************** istema de dos numeros pares he impares **********************************'''
 
-num = int(input("digite un numero: "))
-num1 = int(input("digite otro numero: "))
+# num = int(input("digite un numero: "))
+# num1 = int(input("digite otro numero: "))
 
-result = num / num1 
+# result = num / num1 
 
-if result %2==0:
-    print(f"el resultado es {result}y cuenta coomo un numeoro PAR")
-else:
-    print(f"el resultado es {result} y cuenta como un numero IMPAR")
+# if result %2==0:
+#     print(f"el resultado es {result} y cuenta coomo un numeoro PAR")
+# else:
+#     print(f"el resultado es {result} y cuenta como un numero IMPAR")
 

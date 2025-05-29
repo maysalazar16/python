8d6f8eb02320be17bfb6fa5b5b7d7b001e9432d0
mairon salazar <maironsalazar16@gmail.com>
2025-05-29 16:23:33 -0500

    se crea un programa quie divide dos numero y muestra su resultado con dos decimales

--- clases/hola.py
+++ clases/hola.py
@@ -10,10 +10,17 @@
 
 ## esta es una operacion matematica donde se van a multiplicar nuemero 
 
-n = int(input("ingresa el numero a multiplicar: " ))
-d = int(input("ingresa el numero a multiplicar: " ))
-result = n*d
+# n = int(input("ingresa el numero a multiplicar: " ))
+# d = int(input("ingresa el numero a multiplicar: " ))
+# result = n*d
 
-print(f"lons numero ingresado fueron {n} y {d} y su multiplicacion da como resultado {result}")
+# print(f"los numero ingresado fueron {n} y {d} y su multiplicacion da como resultado {result}")
 
 #se termina la operacion matematica 
+
+div1 = int(input("ingresa el numero a divivir: " ))
+div2 = int(input("ingresa el segundo a dividir: " ))
+
+result = div1/div2
+
+print(f"los numero ingresado fueron {div1} y {div2} y su multiplicacion da como resultado {result:.2f}")

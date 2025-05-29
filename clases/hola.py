871aff26082cc2d263c11300d81bc7e23aa9ed55
mayron salazar <maironsalazar16@gmail.com>
2025-05-29 15:39:10 -0500

    operacion matematica MULTIPLICACION

--- clases/hola.py
+++ clases/hola.py
@@ -1,8 +1,19 @@
-# esto es una documentacion que se hace en python gitttt 
-## esto es un segundo comentario que se hace 
+# # esto es una documentacion que se hace en python gitttt 
+# ## esto es un segundo comentario que se hace 
 
-print("gooool de colombia ")
-n = 3
-b = 4
-r = n**b
-print(r)
+# print("gooool de colombia ")
+# n = 3
+# b = 4
+# r = n**b
+# print(r)
+
+
+## esta es una operacion matematica donde se van a multiplicar nuemero 
+
+n = int(input("ingresa el numero a multiplicar: " ))
+d = int(input("ingresa el numero a multiplicar: " ))
+result = n*d
+
+print(f"lons numero ingresado fueron {n} y {d} y su multiplicacion da como resultado {result}")
+
+#se termina la operacion matematica 
